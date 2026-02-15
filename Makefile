395c20a374c261d0c48c2e16201b980e33128257
tab <tab@0x09.net>
2026-02-15 18:14:00 -0500

    Add support for libfuse v3
    v2 is still supported as well, this will be automatically detected if
    the fuse3 headers are available.

--- Makefile
+++ Makefile
@@ -2,22 +2,22 @@ INSTALL ?= install
 PREFIX ?= /usr
 CFLAGS := -O3 -std=c11 -Wall -Werror -pedantic -D_POSIX_C_SOURCE=200809L $(CFLAGS)
 
-FUSE_FLAGS = -DFUSE_USE_VERSION=28 -D_FILE_OFFSET_BITS=64
-FUSE_LIB = -lfuse
+FUSE_FLAGS = -D_FILE_OFFSET_BITS=64
 OS=$(shell uname)
 ifeq ($(OS), Darwin)
 	FUSE_LDFLAGS = -L/usr/local/lib
 	ifeq ($(shell [ -e /usr/local/lib/libosxfuse.dylib ] && echo 1), 1)
 		FUSE_FLAGS += -I/usr/local/include/osxfuse
 		FUSE_LIB = -losxfuse
-	else ifeq ($(shell [ -e /usr/local/lib/libfuse.dylib ] && echo 1), 1)
+	else ifeq ($(shell [ -e /usr/local/lib/libfuse.dylib -o -e /usr/local/lib/libfuse3.dylib ] && echo 1), 1)
 		FUSE_FLAGS += -I/usr/local/include
+		FUSE_FLAGS += -Wno-language-extension-token -D_DARWIN_C_SOURCE
 	else ifeq ($(shell [ -e /usr/local/lib/libfuse-t.dylib ] && echo 1), 1)
 		FUSE_FLAGS += -I/usr/local/include/fuse
 		FUSE_LIB = -lfuse-t
 	endif
 else
-	CFLAGS := -fPIC $(CFLAGS)
+	CFLAGS := -D_GNU_SOURCE -fPIC $(CFLAGS)
 endif
 ifneq ($(filter $(OS),FreeBSD DragonFly),)
 	FUSE_FLAGS += -I/usr/local/include
@@ -25,6 +25,17 @@ ifneq ($(filter $(OS),FreeBSD DragonFly),)
 else ifeq ($(OS), NetBSD)
 	FUSE_FLAGS += -I/usr/pkg/include
 	FUSE_LDFLAGS = -L/usr/pkg/lib/ -Wl,-R/usr/pkg/lib
+endif
+
+ifeq ($(shell echo | $(CC) $(FUSE_FLAGS) -DFUSE_USE_VERSION=30 -include fuse3/fuse.h -xc -fsyntax-only - > /dev/null 2> /dev/null; echo $$?), 0)
+	FUSE_FLAGS += -DFUSE_USE_VERSION=35
+	FUSE_LIB ?= -lfuse3
+else
+	FUSE_FLAGS += -DFUSE_USE_VERSION=29
+	FUSE_LIB ?= -lfuse
+endif
+ifeq ($(shell echo 'struct statx _;' | $(CC) $(CFLAGS) -include sys/stat.h -xc -fsyntax-only - > /dev/null 2> /dev/null; echo $$?), 0)
+	FUSE_FLAGS += -DHAVE_STATX
 endif
 
 CPPFLAGS := -Iinclude $(FUSE_FLAGS) $(CPPFLAGS)
--- src/fuse_stubs.h
+++ src/fuse_stubs.h
@@ -6,6 +6,22 @@
 // so we try to pass all others along. This set of operations is complete as of FUSE version 2.9.1.
 
 #ifdef __APPLE__
+static int idmapfuse_setvolname(const char *volname) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_setvolname(fs, volname);
+}
+
+static int idmapfuse_setxattr(const char *path, const char *name, const char *value, size_t size, int flags, uint32_t position) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_setxattr(fs, path, name, value, size, flags, position);
+}
+
+static int idmapfuse_getxattr(const char *path, const char *name, char *value, size_t size, uint32_t position) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_getxattr(fs, path, name, value, size, position);
+}
+
+#if FUSE_VERSION < 30
 static int idmapfuse_setattr_x(const char *path, struct setattr_x *attr) {
 	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
 	return fuse_fs_setattr_x(fs, path, attr);
@@ -14,11 +30,6 @@ static int idmapfuse_setattr_x(const char *path, struct setattr_x *attr) {
 static int idmapfuse_fsetattr_x(const char *path, struct setattr_x *attr, struct fuse_file_info *fi) {
 	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
 	return fuse_fs_fsetattr_x(fs, path, attr, fi);
-}
-
-static int idmapfuse_setvolname(const char *volname) {
-	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
-	return fuse_fs_setvolname(fs, volname);
 }
 
 static int idmapfuse_exchange(const char *oldpath, const char *newpath, unsigned long flags) {
@@ -50,17 +61,27 @@ static int idmapfuse_setcrtime(const char *path, const struct timespec *tv) {
 	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
 	return fuse_fs_setcrtime(fs, path, tv);
 }
-static int idmapfuse_setxattr(const char *path, const char *name, const char *value, size_t size, int flags, uint32_t position) {
+
+static int idmapfuse_statfs(const char *path, struct statvfs *buf) {
 	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
-	return fuse_fs_setxattr(fs, path, name, value, size, flags, position);
+	return fuse_fs_statfs(fs, path, buf);
+}
+#else /* FUSE_VERSION >= 30 */
+static int idmapfuse_chflags(const char *path, struct fuse_file_info *fi, uint32_t flags) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_chflags(fs, path, fi, flags);
 }
 
-static int idmapfuse_getxattr(const char *path, const char *name, char *value, size_t size, uint32_t position) {
+static int idmapfuse_statfs(const char *path, struct statfs *buf) {
 	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
-	return fuse_fs_getxattr(fs, path, name, value, size, position);
+	return fuse_fs_statfs(fs, path, buf);
 }
-
+#endif
 #else /* !__APPLE__ */
+static int idmapfuse_statfs(const char *path, struct statvfs *buf) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_statfs(fs, path, buf);
+}
 
 static int idmapfuse_setxattr(const char *path, const char *name, const char *value, size_t size, int flags) {
 	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
@@ -73,10 +94,86 @@ static int idmapfuse_getxattr(const char *path, const char *name, char *value, s
 }
 #endif /* __APPLE__ */
 
+
+#if FUSE_VERSION < 30
 static int idmapfuse_rename(const char *oldpath, const char *newpath) {
 	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
 	return fuse_fs_rename(fs, oldpath, newpath);
 }
+
+static int idmapfuse_utimens(const char *path, const struct timespec tv[2]) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_utimens(fs, path, tv);
+}
+
+static int idmapfuse_chmod(const char *path, mode_t mode) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_chmod(fs, path, mode);
+}
+static int idmapfuse_ftruncate(const char *path, off_t size, struct fuse_file_info *fi) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_ftruncate(fs, path, size, fi);
+}
+
+static int idmapfuse_truncate(const char *path, off_t size) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_truncate(fs, path, size);
+}
+
+static void *idmapfuse_init(struct fuse_conn_info *conn) {
+	struct idmapfuse *ctx = fuse_get_context()->private_data;
+	fuse_fs_init(ctx->next, conn);
+	return ctx;
+}
+#else /* FUSE_VERSION >= 30 */
+static int idmapfuse_rename(const char *oldpath, const char *newpath, unsigned int flags) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_rename(fs, oldpath, newpath, flags);
+}
+
+static int idmapfuse_utimens(const char *path, const struct timespec tv[2], struct fuse_file_info *fi) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_utimens(fs, path, tv, fi);
+}
+
+static int idmapfuse_chmod(const char *path, mode_t mode, struct fuse_file_info *fi) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_chmod(fs, path, mode, fi);
+}
+
+static int idmapfuse_truncate(const char *path, off_t size, struct fuse_file_info *fi) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_truncate(fs, path, size, fi);
+}
+
+static ssize_t idmapfuse_copy_file_range(const char *path_in, struct fuse_file_info *fi_in, off_t offset_in, const char *path_out, struct fuse_file_info *fi_out, off_t offset_out, size_t size, int flags) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_copy_file_range(fs, path_in, fi_in, offset_in, path_out, fi_out, offset_out, size, flags);
+}
+
+static off_t idmapfuse_lseek(const char *path, off_t off, int whence, struct fuse_file_info *fi) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_lseek(fs, path, off, whence, fi);
+}
+
+static void *idmapfuse_init(struct fuse_conn_info *conn, struct fuse_config *cfg) {
+	struct idmapfuse *ctx = fuse_get_context()->private_data;
+	fuse_fs_init(ctx->next, conn, cfg);
+	return ctx;
+}
+#endif
+
+#if FUSE_VERSION < 35
+static int idmapfuse_ioctl(const char *path, int cmd, void *arg, struct fuse_file_info *fi, unsigned int flags, void *data) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_ioctl(fs, path, cmd, arg, fi, flags, data);
+}
+#else
+static int idmapfuse_ioctl(const char *path, unsigned int cmd, void *arg, struct fuse_file_info *fi, unsigned int flags, void *data) {
+	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
+	return fuse_fs_ioctl(fs, path, cmd, arg, fi, flags, data);
+}
+#endif
 
 static int idmapfuse_unlink(const char *path) {
 	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
@@ -138,19 +235,9 @@ static int idmapfuse_flush(const char *path, struct fuse_file_info *fi) {
 	return fuse_fs_flush(fs, path, fi);
 }
 
-static int idmapfuse_statfs(const char *path, struct statvfs *buf) {
-	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
-	return fuse_fs_statfs(fs, path, buf);
-}
-
 static int idmapfuse_opendir(const char *path, struct fuse_file_info *fi) {
 	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
 	return fuse_fs_opendir(fs, path, fi);
-}
-
-static int idmapfuse_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t off, struct fuse_file_info *fi) {
-	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
-	return fuse_fs_readdir(fs, path, buf,  filler, off, fi);
 }
 
 static int idmapfuse_fsyncdir(const char *path, int datasync, struct fuse_file_info *fi) {
@@ -176,26 +263,6 @@ static int idmapfuse_lock(const char *path, struct fuse_file_info *fi, int cmd, 
 static int idmapfuse_flock(const char *path, struct fuse_file_info *fi, int op) {
 	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
 	return fuse_fs_flock(fs, path, fi, op);
-}
-
-static int idmapfuse_chmod(const char *path, mode_t mode) {
-	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
-	return fuse_fs_chmod(fs, path, mode);
-}
-
-static int idmapfuse_truncate(const char *path, off_t size) {
-	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
-	return fuse_fs_truncate(fs, path, size);
-}
-
-static int idmapfuse_ftruncate(const char *path, off_t size, struct fuse_file_info *fi) {
-	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
-	return fuse_fs_ftruncate(fs, path, size, fi);
-}
-
-static int idmapfuse_utimens(const char *path, const struct timespec tv[2]) {
-	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
-	return fuse_fs_utimens(fs, path, tv);
 }
 
 static int idmapfuse_access(const char *path, int mask) {
@@ -233,11 +300,6 @@ static int idmapfuse_bmap(const char *path, size_t blocksize, uint64_t *idx) {
 	return fuse_fs_bmap(fs, path, blocksize, idx);
 }
 
-static int idmapfuse_ioctl(const char *path, int cmd, void *arg, struct fuse_file_info *fi, unsigned int flags, void *data) {
-	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
-	return fuse_fs_ioctl(fs, path, cmd, arg, fi, flags, data);
-}
-
 static int idmapfuse_poll(const char *path, struct fuse_file_info *fi, struct fuse_pollhandle *ph, unsigned *reventsp) {
 	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
 	return fuse_fs_poll(fs, path, fi, ph, reventsp);
@@ -246,10 +308,4 @@ static int idmapfuse_poll(const char *path, struct fuse_file_info *fi, struct fu
 static int idmapfuse_fallocate(const char *path, int mode, off_t offset, off_t length, struct fuse_file_info *fi) {
 	struct fuse_fs *fs = ((struct idmapfuse*)fuse_get_context()->private_data)->next;
 	return fuse_fs_fallocate(fs, path, mode, offset, length, fi);
-}
-
-static void* idmapfuse_init(struct fuse_conn_info *conn) {
-	struct idmapfuse* ctx = fuse_get_context()->private_data;
-	fuse_fs_init(ctx->next, conn);
-	return ctx;
 }
